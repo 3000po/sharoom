a7404a49d0f07db450d8f1268b1f20f5dbaf0fca
greatsk55 <greatsk553@naver.com>
2016-02-11 17:37:53 +0900

    Add Package

---
+++ app/src/main/java/kr/sam1000po/sharoom/activity/Activity_group.java
@@ -0,0 +1,14 @@
+package kr.sam1000po.sharoom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_group extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+    }
+
+}

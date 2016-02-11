575cafa91fce14ef9b738d517f90b666b35e7314
greatsk55 <greatsk553@naver.com>
2016-02-11 12:32:56 +0900

    slack test

--- app/src/main/java/kr/sam1000po/sharoom/activity/MainActivity.java
+++ app/src/main/java/kr/sam1000po/sharoom/activity/MainActivity.java
@@ -1,7 +1,13 @@
 package kr.sam1000po.sharoom;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.util.Log;
 import android.util.Range;
 import android.view.Menu;
@@ -9,6 +15,8 @@ import android.view.MenuItem;
 
 import com.squareup.timessquare.CalendarPickerView;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -28,8 +36,21 @@ public class MainActivity extends Activity {
         //calendar.init(today, nextYear.getTime())
         //        .withSelectedDate(today);
 
+        calendar.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(Date date) {
+
+            }
+
+            @Override
+            public void onDateUnselected(Date date) {
+
+            }
+        });
+
         calendar.init(today, nextYear.getTime())
                 .inMode(CalendarPickerView.SelectionMode.RANGE);
-        calendar.
     }
+
+
 }

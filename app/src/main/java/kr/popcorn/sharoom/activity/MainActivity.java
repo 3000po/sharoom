92b74d19530b000319fdad92da9434122dbfdd40
cwaw72 <cwaw@naver.com>
2016-03-02 01:08:56 +0900

    add main activity and fix the google.lib

--- app/src/main/java/kr/popcorn/sharoom/activity/MainActivity.java
+++ app/src/main/java/kr/popcorn/sharoom/activity/MainActivity.java
@@ -16,19 +16,19 @@ public class MainActivity extends TabActivity {
 
         TabHost tabHost = getTabHost();
 
-        TabSpec tabSpecTab1 = tabHost.newTabSpec("TAB1").setIndicator("강아지");
+        TabSpec tabSpecTab1 = tabHost.newTabSpec("TAB1").setIndicator("방 리스트");
         tabSpecTab1.setContent(R.id.tab1);
         tabHost.addTab(tabSpecTab1);
 
-        TabSpec tabSpecTab2 = tabHost.newTabSpec("TAB2").setIndicator("고양이");
+        TabSpec tabSpecTab2 = tabHost.newTabSpec("TAB2").setIndicator("지도");
         tabSpecTab2.setContent(R.id.tab2);
         tabHost.addTab(tabSpecTab2);
 
-        TabSpec tabSpecTab3 = tabHost.newTabSpec("TAB3").setIndicator("토끼");
+        TabSpec tabSpecTab3 = tabHost.newTabSpec("TAB3").setIndicator("예약한 방");
         tabSpecTab3.setContent(R.id.tab3);
         tabHost.addTab(tabSpecTab3);
 
-        TabSpec tabSpecTab4 = tabHost.newTabSpec("TAB4").setIndicator("말");
+        TabSpec tabSpecTab4 = tabHost.newTabSpec("TAB4").setIndicator("My Page");
         tabSpecTab4.setContent(R.id.tab1);
         tabHost.addTab(tabSpecTab4);
 

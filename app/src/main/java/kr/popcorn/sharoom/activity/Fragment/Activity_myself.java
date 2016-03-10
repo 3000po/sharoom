a484947d4253260cceee51c3a9bfe24b5cc9dc5d
cwaw72 <cwaw@naver.com>
2016-03-10 15:42:40 +0900

    delete main_activity1, 2 and layout

---
+++ app/src/main/java/kr/popcorn/sharoom/activity/Fragment/Activity_myself.java
@@ -0,0 +1,16 @@
+package kr.popcorn.sharoom.activity.Fragment;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import kr.popcorn.sharoom.R;
+
+public class Activity_myself extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myself);
+    }
+
+}

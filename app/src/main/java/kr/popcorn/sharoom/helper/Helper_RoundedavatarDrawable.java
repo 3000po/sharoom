85c42f432272c2cc75d8007773dcbcf2c24a3c01
cwaw72 <cwaw@naver.com>
2016-03-11 10:20:56 +0900

    Helper_Round

--- app/src/main/java/kr/popcorn/sharoom/helper/Helper_RoundedavatarDrawable.java
+++ app/src/main/java/kr/popcorn/sharoom/helper/Helper_RoundedavatarDrawable.java
@@ -16,6 +16,7 @@ import android.graphics.drawable.Drawable;
  */
 
 //안드로이드의 Drawable을 이용해서 이미지가  draw될때 이미지를 라운딩 처리해서 그리는 방법이다.
+    //http://www.kmshack.kr/2013/07/%ED%9A%A8%EC%9C%A8%EC%A0%81%EC%9D%B8-bitmap-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EB%9D%BC%EC%9A%B4%EB%94%A9-%EC%B2%98%EB%A6%AC%EB%B0%A9%EB%B2%95/
 public class Helper_RoundedavatarDrawable extends Drawable{
     private final Bitmap mBitmap;
     private final Paint mPaint;

92bc225b5284fee29c8773aedec40a380e2fc247
parknature <parknature1013@hanmanil.net>
2016-03-14 15:21:25 +0900

    i am park

---
+++ app/src/main/java/kr/popcorn/sharoom/activity/Fragment/Activity_map.java
@@ -0,0 +1,15 @@
+package kr.popcorn.sharoom.activity.Fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import kr.popcorn.sharoom.R;
+
+public class Activity_map extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
---
+++ app/src/main/java/kr/popcorn/sharoom/activity/Fragment/MyApplication.java
@@ -0,0 +1,22 @@
+package kr.popcorn.sharoom.activity.Fragment;
+
+import android.app.Application;
+
+/**
+ * Created by parknature on 16. 3. 7..
+ */
+public class MyApplication extends Application {
+    private String mGlobalString;
+
+    public String getGlobalString()
+    {
+        return mGlobalString;
+    }
+
+    public void setGlobalString(String globalString)
+    {
+        this.mGlobalString = globalString;
+    }
+
+
+}

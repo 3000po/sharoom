d39b9aa0b67731ed4297506988d5cd44b298ce54
cwaw72 <cwaw@naver.com>
2016-03-19 14:26:46 +0900

    add the tabview_myselfactivity

--- app/src/main/java/kr/popcorn/sharoom/activity/TabView/TabView_myselfAdapter.java
+++ app/src/main/java/kr/popcorn/sharoom/activity/TabView/TabView_myselfAdapter.java
@@ -1,5 +1,6 @@
 package kr.popcorn.sharoom.activity.TabView;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -9,30 +10,109 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import kr.popcorn.sharoom.R;
+import kr.popcorn.sharoom.helper.Helper_roomData;
 
-public class TabView_myselfAdapter extends Activity {
+public class TabView_myselfAdapter extends RecyclerView.Adapter<TabView_myselfAdapter.ViewHolder> {
 
     ImageView myface;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_myself);
+    private Context mContext;
+    public ArrayList<Helper_roomData> list;
+    private LinearLayoutManager linearLayoutManager;
 
-        myface= (ImageView)findViewById(R.id.myface);
-        myface.getDrawable();
-        init();
-
+    public ArrayList<Helper_roomData> getContactsList() {
+        return list;
     }
 
+    public TabView_myselfAdapter(Context context, ArrayList<Helper_roomData> _dataSet, LinearLayoutManager linearLayoutManager) {
+        mContext = context;
+        list = _dataSet;
+        this.linearLayoutManager = linearLayoutManager;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext)
+                .inflate(R.layout.activity_myself, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        Bitmap face = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.myself_50x50);
+        //myface.setImageBitmap(getCircleBitmap(face));
+
+        //holder.myface.setImageResource(R.drawable.ic_action_mapview_m);
+        holder.myface.setImageBitmap(getCircleBitmap(face));
+        holder.myname.setText(list.get(position).roomname);
+        //holder.text.setText(tmp.substring(0,4));
+    }
+
+    @Override
+    public int getItemCount() {
+        if (list == null) return 0;
+        return list.size();
+    }
+
+    public void add(Helper_roomData song, int position) {
+        list.add(position, song);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position) {
+        list.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public ImageView myface;
+        public TextView myname;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            //album = (ImageView) itemView.findViewById(R.id.album_art1);
+            //text = (TextView) itemView.findViewById(R.id.year);
+
+            myface = (ImageView) itemView.findViewById(R.id.myface);
+            myname = (TextView) itemView.findViewById(R.id.myname);
+
+            itemView.setClickable(true);
+            itemView.setOnClickListener(this);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    // song is selected
+                    return true;
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View v) {
+        }
+
+    }
     void init(){
 
-        Bitmap face = BitmapFactory.decodeResource(getBaseContext().getResources(),R.drawable.myself_50x50);
-        
-        myface.setImageBitmap(getCircleBitmap(face));
+        //myface= (ImageView)findViewById(R.id.myface);
+       // myface.getDrawable();
+       // init();
+       // Bitmap face = BitmapFactory.decodeResource(getBaseContext().getResources(),R.drawable.myself_50x50);
+
+        //myface.setImageBitmap(getCircleBitmap(face));
 
     }
 

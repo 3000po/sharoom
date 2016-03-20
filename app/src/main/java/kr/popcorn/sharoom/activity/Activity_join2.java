ad6d8040f2e064a5e21a3fdcae6113176d4f3958
kisang <whrltkd789@naver.com>
2016-03-21 01:20:27 +0900

    Activity_join2 source modify and add alert and backPress

--- app/src/main/java/kr/popcorn/sharoom/activity/Activity_join2.java
+++ app/src/main/java/kr/popcorn/sharoom/activity/Activity_join2.java
@@ -94,15 +94,14 @@ public class Activity_join2 extends Activity {
         form_basic.et_id.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus == false){
+                if (hasFocus == false) {
                     RequestParams idParams = new RequestParams();
                     String id = form_basic.et_id.getText().toString();
                     idParams.put("id", id);
                     if (!Helper_checker.validId_context(Activity_join2.this, id)) {
                         id_check_ok = false;
                         return;
-                    }
-                    else {
+                    } else {
                         Helper_server.post("idCheck.php", idParams, new JsonHttpResponseHandler() {
                             @Override
 
@@ -134,8 +133,7 @@ public class Activity_join2 extends Activity {
                             }
                         });
                     }
-                }
-                else{
+                } else {
                     form_basic.tv_idCheck.setText("");
                     id_check_ok = false;
                 }
@@ -145,7 +143,7 @@ public class Activity_join2 extends Activity {
         View.OnClickListener requestJoin = new View.OnClickListener() {
             public void onClick(View v) {
 
-                if(!Helper_checker.id_check_ok(Activity_join2.this, id_check_ok)){
+                if (!Helper_checker.id_check_ok(Activity_join2.this, id_check_ok)) {
                     return;
                 }
 
@@ -159,23 +157,11 @@ public class Activity_join2 extends Activity {
                     String phoneNumber = form_basic.et_phoneNumber.getText().toString();
                     String email = form_basic.et_email.getText().toString();
 
-//                    if (!Helper_checker.validJoin(Activity_join2.this, email, name, id, password)) {
-//                        return;
-//                    }
-//
-//                    if (!Helper_checker.id_check_ok(Activity_join2.this, id_check_ok)) {
-//                        return;
-//                    }
-                    Log.i("Msg", "id : " + id + "pwd : " + password + "name : " + name + "phoneNumber : " + phoneNumber + "email : " + email);
-
-//                    id = urlEncodeUTF8(id);
-//                    password = urlEncodeUTF8(password);
-//                    name = urlEncodeUTF8(name);
-//                    phoneNumber = urlEncodeUTF8(phoneNumber);
-//                    email = urlEncodeUTF8(email);
+                    if (!Helper_checker.validJoin(Activity_join2.this, email, name, id, password)) {
+                        return;
+                    }
 
                     Log.i("Msg", "id : " + id + "pwd : " + password + "name : " + name + "phoneNumber : " + phoneNumber + "email : " + email);
-                    join_flag = true;
 
                     params.put("id", id);
                     params.put("password", password);
@@ -188,10 +174,7 @@ public class Activity_join2 extends Activity {
 
                         public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                             Log.i("Msg", "success");
-                            Intent intent = new Intent(Activity_join2.this, Activity_login2.class);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                            startActivity(intent);
-                            finish();
+                            joinAlert();
                         }
 
                         @Override
@@ -207,17 +190,17 @@ public class Activity_join2 extends Activity {
         form_basic.btn_submit.setOnClickListener(requestJoin);
 
         //email 입력후 엔터눌렀을때 회원가입 처리
-        form_basic.et_email.setOnKeyListener(new View.OnKeyListener()
-
-                                             {
+        form_basic.et_email.setOnKeyListener(new View.OnKeyListener() {
                                                  @Override
-                                                 public boolean onKey (View v,int keyCode, KeyEvent event){
+                                                 public boolean onKey(View v, int keyCode, KeyEvent event) {
                                                      //Enter key Action
                                                      if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
                                                          InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
                                                          imm.hideSoftInputFromWindow(form_basic.et_email.getWindowToken(), 0);    //hide keyboard
 
                                                          if (network_check.isNetWork()) {
+                                                             RequestParams params = new RequestParams();
+                                                             Log.d("idcheck", "" + id_check_ok);
                                                              String id = form_basic.et_id.getText().toString();
                                                              String password = form_basic.et_password.getText().toString();
                                                              String name = form_basic.et_name.getText().toString();
@@ -228,17 +211,27 @@ public class Activity_join2 extends Activity {
                                                                  return false;
                                                              }
 
-                                                             if (!Helper_checker.id_check_ok(Activity_join2.this, id_check_ok)) {
-                                                                 return false;
-                                                             }
+                                                             Log.i("Msg", "id : " + id + "pwd : " + password + "name : " + name + "phoneNumber : " + phoneNumber + "email : " + email);
 
-                                                             id = urlEncodeUTF8(id);
-                                                             password = urlEncodeUTF8(password);
-                                                             name = urlEncodeUTF8(name);
-                                                             phoneNumber = urlEncodeUTF8(phoneNumber);
-                                                             email = urlEncodeUTF8(email);
+                                                             params.put("id", id);
+                                                             params.put("password", password);
+                                                             params.put("name", name);
+                                                             params.put("phoneNumber", phoneNumber);
+                                                             params.put("email", email);
 
-                                                             join_flag = true;
+                                                             Helper_server.post("member_Insert.php", params, new AsyncHttpResponseHandler() {
+                                                                 @Override
+
+                                                                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                                                     Log.i("Msg", "success");
+                                                                     joinAlert();
+                                                                 }
+
+                                                                 @Override
+                                                                 public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                                                     Log.i("Msg", "fali");
+                                                                 }
+                                                             });
                                                          }
                                                          return true;
                                                      }
@@ -247,113 +240,23 @@ public class Activity_join2 extends Activity {
                                              }
 
         );
+    } //onCreate 종료
+
+    public void joinAlert() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(Activity_join2.this);
+        alert.setTitle("성공");
+        alert.setMessage("가입이 완료되셨습니다.");
+        alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Activity_join2.this, Activity_login2.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+                finish();
+            }
+        });
+        alert.show();
     }
 
-    private void downloadUrl(String php) throws IOException {
-        StringBuilder jsonHtml = new StringBuilder();
-        InputStream iStream = null;
-        String line="";
-        String id = "";
-        BufferedReader br = null;
-        Boolean ok = false;
-
-        try {
-            URL url = new URL(php);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.connect();
-            iStream = urlConnection.getInputStream();
-
-            if(id_check_ok == false) {
-                br = new BufferedReader(new InputStreamReader(iStream));
-                while ((line = br.readLine()) != null) {
-                    jsonHtml.append(line + "\n");
-                }
-
-                br.close();
-                id = jsonHtml.toString();
-                iStream.close();
-                urlConnection.disconnect();
-                JSONObject root = new JSONObject(id);
-                id = root.getString("id");
-
-                if(id.equals("valid")) {
-                    id_check_ok = true;
-                }
-                else if(id.equals("invalid")){
-                    id_check_ok = false;
-                }
-            }
-        } catch(Exception e){
-            Log.d("Exception ", e.toString());
-
-        }
-    }
-
-    private class phpTask extends AsyncTask<String, Integer, String> {
-        protected String doInBackground(String... url) {
-            try {
-                Log.d("url", url[0].toString());
-                downloadUrl(url[0]);
-            } catch (Exception e) {
-                Log.d("Background Task", e.toString());
-            }
-            return null;
-        }
-
-        protected void onPostExecute(String result) {
-
-            if (join_flag == true && id_check_ok == true) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(Activity_join2.this);
-                alert.setTitle("성공");
-                alert.setMessage("가입이 완료되셨습니다.");
-                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        Intent intent = new Intent(Activity_join2.this, Activity_login.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                        startActivity(intent);
-                        finish();
-                    }
-                });
-                alert.show();
-            }
-            else if(join_flag == true && id_check_ok == false){
-                AlertDialog.Builder alert = new AlertDialog.Builder(Activity_join2.this);
-                alert.setTitle("가입");
-                alert.setMessage("아이디 중복검사를 하지 않았습니다.");
-                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                });
-                alert.show();
-            }
-
-            else if(id_check_ok == false){
-                AlertDialog.Builder alert = new AlertDialog.Builder(Activity_join2.this);
-                alert.setTitle("ID검사");
-                alert.setMessage("사용할수 없는 아이디입니다.");
-                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-                alert.show();
-            }
-            else if(id_check_ok == true){
-                AlertDialog.Builder alert = new AlertDialog.Builder(Activity_join2.this);
-                alert.setTitle("ID검사");
-                alert.setMessage("사용할수 있는 아이디입니다.");
-                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-                alert.show();
-            }
-
-
-            join_flag = false;
-        }
-    }
     public static String urlEncodeUTF8(String url) {
         try {
             return URLEncoder.encode(url, "UTF-8");
@@ -378,4 +281,12 @@ public class Activity_join2 extends Activity {
         // Logs 'app deactivate' App Event.
         AppEventsLogger.deactivateApp(this);
     }
+
+    public void onBackPressed(){
+        Intent intent = new Intent(Activity_join2.this, Activity_login2.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
+        finish();
+    }
+
 }

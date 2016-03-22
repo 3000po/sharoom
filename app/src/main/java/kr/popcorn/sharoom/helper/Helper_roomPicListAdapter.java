34d1a657c13d4df3f8047f63632c5bbb4fa42cd8
parknature <parknature1013@hanmanil.net>
2016-03-23 02:40:19 +0900

    add image, xml file in drawable

--- app/src/main/java/kr/popcorn/sharoom/helper/Helper_roomPicListAdapter.java
+++ app/src/main/java/kr/popcorn/sharoom/helper/Helper_roomPicListAdapter.java
@@ -1,8 +1,6 @@
 package kr.popcorn.sharoom.helper;
 
-import android.app.FragmentTransaction;
 import android.content.Context;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 

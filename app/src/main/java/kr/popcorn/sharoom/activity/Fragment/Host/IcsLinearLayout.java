f2d1cbe8aae0e3adc4157ad424cc8cbb2e1efdab
greatsk55 <greatsk553@naver.com>
2016-05-09 17:26:29 +0900

    ..

--- app/src/main/java/kr/popcorn/sharoom/activity/Fragment/Host/IcsLinearLayout.java
+++ app/src/main/java/kr/popcorn/sharoom/activity/Fragment/Host/IcsLinearLayout.java
@@ -31,11 +31,12 @@ class IcsLinearLayout extends LinearLayout {
     private int mDividerPadding;
 
 
+    @SuppressWarnings("ResourceType")
     public IcsLinearLayout(Context context, int themeAttr) {
         super(context);
 
         TypedArray a = context.obtainStyledAttributes(null, LL, themeAttr, 0);
-        setDividerDrawable(a.getDrawable(kr.popcorn.sharoom.activity.Fragment.Host.IcsLinearLayout.LL_DIVIDER));
+        setDividerDrawable(a.getDrawable(IcsLinearLayout.LL_DIVIDER));
         mDividerPadding = a.getDimensionPixelSize(LL_DIVIDER_PADDING, 0);
         mShowDividers = a.getInteger(LL_SHOW_DIVIDER, SHOW_DIVIDER_NONE);
         a.recycle();
